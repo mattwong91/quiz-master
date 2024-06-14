2938343fe308692af34772a8a056cf3833f98069
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-14 14:23:14 -0600

    Incorrect answers display message and show correct answer

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,14 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
   [SerializeField] TextMeshProUGUI questionText;
   [SerializeField] QuestionSO question;
   [SerializeField] GameObject[] answerButtons;
+  int correctAnswerIndex;
+  [SerializeField] Sprite defaultAnswerSprite;
+  [SerializeField] Sprite correctAnswerSprite;
 
   void Start()
+  {
+    correctAnswerIndex = question.GetCorrectAnswerIndex();
+    DisplayQuestion();
+  }
+
+  public void OnAnswerSelected(int index)
+  {
+    Image buttonImage;
+
+    if (index == question.GetCorrectAnswerIndex())
+    {
+      questionText.text = "Correct!";
+      buttonImage = answerButtons[index].GetComponent<Image>();
+      buttonImage.sprite = correctAnswerSprite;
+    }
+    else
+    {
+      string correctAnswer = question.GetAnswer(correctAnswerIndex);
+      questionText.text = "Incorrect! The answer was:\n" + correctAnswer;
+
+      buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+      buttonImage.sprite = correctAnswerSprite;
+    }
+  }
+
+  void DisplayQuestion()
   {
     questionText.text = question.GetQuestion();
 
@@ -19,5 +49,4 @@ public class Quiz : MonoBehaviour
       buttonText.text = question.GetAnswer(i);
     }
   }
-
 }
