dd1c25093bd212a50bd3f7de138014ebf9008e14
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-14 17:47:16 -0600

    Add fillFraction, add timer cancel

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,7 +7,9 @@ public class Timer : MonoBehaviour
   [SerializeField] float timeToCompleteQuestion = 30f;
   [SerializeField] float timeToShowCorrectAnswer = 10f;
 
+  public bool loadNextQuestion;
   public bool isAnsweringQuestion = false;
+  public float fillFraction;
 
   float timerValue;
 
@@ -16,13 +18,22 @@ public class Timer : MonoBehaviour
     UpdateTimer();
   }
 
+  public void CancelTimer()
+  {
+    timerValue = 0;
+  }
+
   void UpdateTimer()
   {
     timerValue -= Time.deltaTime;
 
     if (isAnsweringQuestion)
     {
-      if (timerValue <= 0)
+      if (timerValue > 0)
+      {
+        fillFraction = timerValue / timeToCompleteQuestion;
+      }
+      else
       {
         isAnsweringQuestion = false;
         timerValue = timeToShowCorrectAnswer;
@@ -30,10 +41,15 @@ public class Timer : MonoBehaviour
     }
     else
     {
-      if (timerValue <= 0)
+      if (timerValue > 0)
+      {
+        fillFraction = timerValue / timeToShowCorrectAnswer;
+      }
+      else
       {
         isAnsweringQuestion = true;
         timerValue = timeToCompleteQuestion;
+        loadNextQuestion = true;
       }
     }
   }
