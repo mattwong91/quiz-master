48d09c35dca9c6854f694fa9715bb11e4600f549
Matthew Wong <144198400+mattwong91@users.noreply.github.com>
2024-06-17 22:22:12 -0600

    Avoid null reference errors

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -33,7 +33,7 @@ public class Quiz : MonoBehaviour
 
   public bool isComplete;
 
-  void Start()
+  void Awake()
   {
     timer = FindObjectOfType<Timer>();
     scoreKeeper = FindObjectOfType<ScoreKeeper>();
@@ -46,6 +46,11 @@ public class Quiz : MonoBehaviour
     timerImage.fillAmount = timer.fillFraction;
     if (timer.loadNextQuestion)
     {
+      if (progressBar.value == progressBar.maxValue)
+      {
+        isComplete = true;
+        return;
+      }
       hasAnsweredEarly = false;
       GetNextQuestion();
       timer.loadNextQuestion = false;
@@ -64,11 +69,6 @@ public class Quiz : MonoBehaviour
     SetButtonState(false);
     timer.CancelTimer();
     scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
-
-    if (progressBar.value == progressBar.maxValue)
-    {
-      isComplete = true;
-    }
   }
 
   void DisplayAnswer(int index)
